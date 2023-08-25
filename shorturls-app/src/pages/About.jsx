e771c7f81aca97186fb45b4c8f929d7c9ec53018
Kostiantyn Bulgaka <kostyabulgaka3@gmail.com>
2023-08-25 09:14:17 +0300

    add "about"

--- shorturls-app/src/pages/About.jsx
+++ shorturls-app/src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
   }
   return (
     <div className="containe logup__wrapper is-flex is-flex-direction-column">
-      <p className="question__question content has-text-centered">This site uses Base64 encryption to transform your link into something like: localhost:3000/xyz, while pragamtically mantaining the functionality of the link</p>
+      <p className="question__question content has-text-centered">This web app uses Base64 encryption to transform your link into something like: localhost:3000/xyz, while pragamtically mantaining the functionality of the link</p>
       <button
         type="button"
         className="button is-danger title is-5  btn"
